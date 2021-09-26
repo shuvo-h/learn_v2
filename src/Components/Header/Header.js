d7c3a616d29841e2aec5e59a585a1ab5772f4a86
shuvo-h <shuvowriter@gmail.com>
2021-09-26 17:53:14 +0600

    comment added on css

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './header.css';
 import Banner from '../../images/banner.png';
+import Banner2 from '../../images/banner-2.png';
 
 const Header = () => {
 
     return (
         <div className='header-container'>
+                <img className='banner banner-1' src={Banner} alt="" />
             <div>
                 <h1>Asian Satelite 2</h1>
                 <h2 className='project-budget'>Total Budget $800 million</h2>
             </div>
-            <img className='banner' src={Banner} alt="" />
+            
+                <img className='banner' src={Banner2} alt="" />
+    
         </div>
     );
 };
